97b8384acf1b0d84a775958310b1aeb817f7df11
fmoran-m <franmoranmato@gmail.com>
2024-10-01 21:48:06 +0200

    Minor fix

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,7 +1,7 @@
 #include "HumanB.hpp"
 #include <iostream>
 
-HumanB::HumanB(std::string name) : _name(name){
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL){
 	return;
 }
 HumanB::~HumanB() {
@@ -13,5 +13,11 @@ void	HumanB::setWeapon(Weapon &weapon){
 }
 
 void	HumanB::attack(void){
+	if (this->_weapon == NULL)
+	{
+		std::cout << this->_name << " needs a weapon!" << std::endl;
+		return;
+	}
 	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	return;
 }
