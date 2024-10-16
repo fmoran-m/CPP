b60c2092370ed1e6b0f2921e1ff3e31b7c5051ad
Francisco Jose Moran Mato <fmoran-m@c1r15s4.42madrid.com>
2024-10-16 14:58:45 +0200

    cpp07 ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,24 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+T const max(T x, T y){
+	return (x > y)? x : y;
+}
+
+template <typename T>
+T min(T x, T y){
+	return (x < y)? x : y;
+}
+
+template <typename T>
+void swap(T &x, T &y){
+	T temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+	return;
+}
+
+#endif
