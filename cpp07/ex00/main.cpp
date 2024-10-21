05ac8aa0361ebf77e1e04d66f006a0e33c5c899c
fmoran-m <franmoranmato@gmail.com>
2024-10-21 15:14:51 +0200

    done cpp07

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -15,5 +15,15 @@ int main(void){
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::string e = "str";
+	std::string f = "string";
+	std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
+	std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
+	char g = 'g';
+	char h = 'h';
+	::swap(g, h);
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min( g, h ) = " << ::min(g, h) << std::endl;
+	std::cout << "max( g, h ) = " << ::max(g, h) << std::endl;
 	return 0;
 }
