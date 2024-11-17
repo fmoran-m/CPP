6443a76512e4ed5f26a0241c1ca0d39b3fe3f4cc
fmoran-m <franmoranmato@gmail.com>
2024-11-17 21:05:18 +0100

    calculating time cpp09

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <climits>
 #include <string>
+#include <sys/time.h>
 
 int main(int argc, char **argv)
 {
@@ -30,26 +31,34 @@ int main(int argc, char **argv)
 		std::cout << "Error: " << e.what() << std::endl;
 	}
 
-	std::cout << "Vector: ";
+	std::cout << "Before: ";
 	for (unsigned int i = 0; i < a.numbersVector.size(); i++)
 		std::cout << a.numbersVector[i] << " ";
 	std::cout << std::endl;
 
-	std::cout << "Deque: ";
-	for (unsigned int i = 0; i < a.numbersDeque.size(); i++)
-		std::cout << a.numbersDeque[i] << " ";
-	std::cout << std::endl;
-
+	struct timeval beforeVectorTime;
+	struct timeval afterVectorTime;
+	gettimeofday(&beforeVectorTime, NULL);
 	a.applyVectorAlgorithm();
-	std::cout << "Vector: ";
+	gettimeofday(&afterVectorTime, NULL);
+
+	std::cout << "After (Vector): ";
 	for (unsigned int i = 0; i < a.numbersVector.size(); i++)
 		std::cout << a.numbersVector[i] << " ";
 	std::cout << std::endl;
 
+	struct timeval beforeDequeTime;
+	struct timeval afterDequeTime;
+	gettimeofday(&beforeDequeTime, NULL);
 	a.applyDequeAlgorithm();
-	std::cout << "Deque: ";
+	gettimeofday(&afterDequeTime, NULL);
+
+	std::cout << "After (Deque): ";
 	for (unsigned int i = 0; i < a.numbersDeque.size(); i++)
 		std::cout << a.numbersDeque[i] << " ";
 	std::cout << std::endl;
+
+	std::cout << "Time to process a range of " << a.numbersVector.size() << " elements with std::vector : " << afterVectorTime.tv_sec - beforeVectorTime.tv_usec << std::endl;
+	std::cout << "Time to process a range of " << a.numbersDeque.size() << " elements with std::deque : " << afterDequeTime.tv_sec - beforeDequeTime.tv_usec << std::endl;
 	return 0;
 }
